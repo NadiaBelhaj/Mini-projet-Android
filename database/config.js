8c6544cc83e87cf18451af3800d9549b7bd59621
NadiaBelhaj <nadia.belhaj@esprit.tn>
2021-12-30 19:41:47 +0100

    Server

---
+++ database/config.js
@@ -0,0 +1,56 @@
+const mysql = require('mysql');
+const schema = require( '../database/schema');
+
+const databaseConfig = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'frenzy',
+    port: '3306'
+};
+
+const connection = mysql.createConnection(databaseConfig);
+
+connection.connect(error => {
+    if (error) throw error;
+
+    console.debug("Open MySQL Connection");
+
+    //Create users table
+    connection.query(schema.MYSQL_USERS_TABLE, (err, res) =>{
+        if (err) throw err;
+        console.log("Users Table Done");
+    });
+
+    //Create questions table
+    connection.query(schema.MYSQL_QUESTIONS_TABLE, (err, res) =>{
+        if (err) throw err;
+        console.log("Questions Table Done");
+    });
+
+    //Create answerd table
+    connection.query(schema.MYSQL_ANSWERD_TABLE, (err, res) =>{
+        if (err) throw err;
+        console.log("Answerd Table Done");
+    });
+
+    //Create follows table
+    connection.query(schema.MYSQL_FOLLOWS_TABLE, (err, res) =>{
+        if (err) throw err;
+        console.log("Follows Table Done");
+    });
+
+    //Create notifications table
+    connection.query(schema.MYSQL_NOTIFICATIONS_TABLE, (err, res) =>{
+        if (err) throw err;
+        console.log("Notifications Table Done");
+    });
+
+    //Create reactions table
+    connection.query(schema.MYSQL_REACTIONS_TABLE, (err, res) =>{
+        if (err) throw err;
+        console.log("Reactions Table Done");
+    });
+});
+
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+const socketIO = require('socket.io')(server);
+
+const socketManager = require('./sockets/socket_manager');
+socketManager.startListener(socketIO);
+
+server.listen(port, () => console.debug("Server start at PORT 8000"));
